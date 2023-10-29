2c73d3b762b4c1bd575487685c1b3d20988f8744
Shahrukh A. Khan <iam@shaz3e.com>
2023-10-29 20:44:45 +0500

    Admin/User CRUD and Profile

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -6,7 +6,6 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Support\Facades\Hash;
 use Laravel\Sanctum\HasApiTokens;
 
 class Admin extends Authenticatable
@@ -22,7 +21,6 @@ class Admin extends Authenticatable
      */
     protected $fillable = [
         'name',
-        'username',
         'email',
         'password',
     ];
